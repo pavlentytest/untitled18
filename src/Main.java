af15ac8ec0d6e82b145a87532e611463fdd453a0
pavlenty <impetus@yandex.ru>
2019-11-09 08:52:01 +0300

    a+b

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        int a = 100;
+        int a = 500;
         int b = 200;
+        System.out.println(a+b);
     }
 }
